d37f127f7389abe51c4aecf17875f0d19bfd16fb
ShivamNolkha <57953639+ShivamNolkha@users.noreply.github.com>
2024-04-04 07:18:02 +0000

    changed devcontainer

---
+++ basics/dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all JS in this document as newer version
+
+// alert(3 + 3) //we are using nodejs , not browser
+
+let name = "shivam"
+let age = 52
+let isLoggedIn = false
+
+
+// null => standalone value hein
+
+console.log(typeof undefined); //returns undefined
+
+console.log(typeof null);  //returns Object
---
+++ basics/variables.js
@@ -0,0 +1,16 @@
+const accountId = 238440;
+let accountEmail = "shivam@gmail.com";
+var accountPassword = "12345";
+accountCity = "bhilwara";
+let accountState;
+//accountId = 2;
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+
+/*
+We prefer not to use VAR because of in block scope or function scope
+*/
